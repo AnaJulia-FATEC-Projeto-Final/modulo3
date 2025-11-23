9f4751f96213478c41b659a7a5edc94d7a45f89c
ajlima12 <ajlimaa12@gmail.com>
2025-11-23 17:24:30 -0300

    adicionando mensagem

---
+++ src/main/java/com/anajulia/modulo3/function/dto/PessoaAtualizadaMensagem.java
@@ -0,0 +1,10 @@
+package com.anajulia.modulo3.function.dto;
+
+import java.time.LocalDate;
+
+public record PessoaAtualizadaMensagem(
+        String nome,
+        LocalDate dt_nascimento
+) {
+
+}

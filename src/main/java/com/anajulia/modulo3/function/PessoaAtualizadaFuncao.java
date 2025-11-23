c8c1c3f03d1ddb16366d25fb9ced50a0d8da44c2
ajlima12 <ajlimaa12@gmail.com>
2025-11-23 17:59:01 -0300

    corrigindo funcao

--- src/main/java/com/anajulia/modulo3/function/PessoaAtualizadaFuncao.java
+++ src/main/java/com/anajulia/modulo3/function/PessoaAtualizadaFuncao.java
@@ -3,13 +3,12 @@ package com.anajulia.modulo3.function;
 import com.anajulia.modulo3.function.dto.PessoaAtualizadaMensagem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
 import java.util.function.Consumer;
 
 @Component
 public class PessoaAtualizadaFuncao {
     @Bean
-    public Consumer<PessoaAtualizadaMensagem> clientRegisteredConsumer() {
+    public Consumer<PessoaAtualizadaMensagem> lambdaConsumer() {
         return consumer -> {
             System.out.println("A mensagem chegou: " + consumer.toString());
         };
